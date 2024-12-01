a1c3fe7c56d2dcc80407b93a795b09adc566aec4
Danish Khan <khandanishe125@gmail.com>
2024-12-01 23:27:11 +0530

    file rename

--- Brave Browser Home page using tailwind/src/App.jsx
+++ Brave Browser Home page using tailwind/src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,23 +7,25 @@ function App() {
   return (
     <>
       <div id="main" className='container'>
-        <div id="addblocker" className='flex justify-between'>
-          <div id="header" className='flex'>
-            <div>
-              <h1>2,271</h1>
-              <p>Tracker and ads blocks</p>
+        <div id="addBlocker" className='flex justify-between mt-8'>
+          <div id="header" className='flex gap-4'>
+            
+            <div className='flex flex-col gap-4 justify-center'>
+              <h1 className=' text-5xl text-orange-500'>3,359</h1>
+              <p className='font-bold '>Bandwidth saved</p>
             </div>
-            <div>
-              <h1>57.7</h1>
-              <p>Bandwidth saved</p>
+            <div className='flex flex-col gap-4 justify-center'>
+              <h1 className=' text-5xl' style={{color:"#9DA3E6"}}>57.7</h1>
+              <p className='font-bold '>Bandwidth saved</p>
             </div>
-            <div>
-              <h1>2<span>minutes</span></h1>
-              <p>Time saved</p>
+            <div className='flex flex-col gap-4 justify-center'>
+              <h1 className=' text-5xl'>2 </h1>
+              <p className='font-bold '>Time saved</p>
             </div>
+            
           </div>
           <div id="clock">
-            <h1>10:09</h1>
+            <h1 className=' text-8xl text-white'>10:09</h1>
           </div>
 
         </div>
